885f4c625cd9b022fb0a5d0c47d98cb46818384e
Alexis Bourget <alexis.bourget@gmail.com>
2021-11-18 20:35:40 +0100

    Use " when possible and immediately source install script when save is done

--- config-init-script.sh
+++ config-init-script.sh
@@ -1,16 +1,18 @@
 # See README.md for source
-mkdir -p $HOME/.config/
-git clone --bare raw.githubusercontent.com/poliorcetics/bare-config.git $HOME/.config/bare-config.git
+mkdir -p "$HOME/.config/"
+git clone --bare raw.githubusercontent.com/poliorcetics/bare-config.git "$HOME/.config/bare-config.git"
 function config {
-   git --git-dir=$HOME/.config/bare-config.git --work-tree=$HOME $@
+   git --git-dir="$HOME/.config/bare-config.git" --work-tree="$HOME" $@
 }
-mkdir -p .config-backup
+mkdir -p "$HOME/.config.bak"
 config checkout
-if [ $? = 0 ]; then
+if [[ $? -eq 0 ]]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} "$HOME/.config.bak/{}"
 fi;
 config checkout
 config config status.showUntrackedFiles no
+
+source "$HOME/.config/shells/common/install.sh"
